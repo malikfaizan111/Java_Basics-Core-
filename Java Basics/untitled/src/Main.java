bd22252c3cbd17c571c7274e5146949dfc9038d9
Faizan Atif <faizan.atif@ircmonline.com>
2023-11-13 10:55:30 +0500

    update java basics

---
+++ Java Basics/untitled/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.PrintStream;
+
+public class Main {
+    public static void main(String... args)    {
+//        System.out.println("Your first argument is: "+args[0]);
+//        try(PrintStream  printStream = new PrintStream(System.out)){
+//            printStream.println("Your first argument is your_variable_or_value");
+//        }
+//        catch (Exception e){
+//            e.printStackTrace();
+//        }
+
+        boolean isAcitve = false;
+        byte a = 10;
+
+    }
+}
