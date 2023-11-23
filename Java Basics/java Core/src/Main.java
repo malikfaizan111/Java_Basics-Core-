05183c97ae44de6532f37bc3e9533cb371264f14
Faizan Atif <faizan.atif@ircmonline.com>
2023-11-23 16:40:15 +0500

    update task

--- Java Basics/java Core/src/Main.java
+++ Java Basics/java Core/src/Main.java
@@ -1,6 +1,7 @@
 //import java.io.PrintStream;
+//import java.util.ArrayList;
 import java.util.Scanner;
-
+import java.util.*; // all things associated with util class
 public class Main {
     public static void main(String... args)    {
 //        System.out.println("Your first argument is: "+args[0]);
@@ -63,20 +64,89 @@ public class Main {
 //        name = scan.nextLine(); // scan.next() only get until the space
 //        System.out.print("Display Name: " + name);
 
-        try (Scanner scan = new Scanner(System.in)) {
-            while (true) {
-                System.out.print("Enter a Name (or type 'exit' to quit): ");
-                String name = scan.nextLine();
+//        try (Scanner scan = new Scanner(System.in)) {
+//            while (true) {
+//                System.out.print("Enter a Name (or type 'exit' to quit): ");
+//                String name = scan.nextLine();
+//
+//                if (name.equalsIgnoreCase("exit")) {
+//                    System.out.println("Exiting the program.");
+//                    break; // Exit the loop if the user types 'exit'
+//                }
+//
+//                System.out.println("Display Name: " + name);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace(); // Print the exception details to the console
+//        }
 
-                if (name.equalsIgnoreCase("exit")) {
-                    System.out.println("Exiting the program.");
-                    break; // Exit the loop if the user types 'exit'
-                }
+//        int x = 10;
+//        int y = 12;
+//
+//        if(x+y > 20){
+//            System.out.println("X + Y is Greator Than 20");
+//        }
+//        else
+//        {
+//            System.out.println("x + y is Less than 20");
+//        }
 
-                System.out.println("Display Name: " + name);
-            }
-        } catch (Exception e) {
-            e.printStackTrace(); // Print the exception details to the console
+//        String city = "Delhi";
+//        if(city == "Meerut") {
+//            System.out.println("city is meerut");
+//        }else if (city == "Noida") {
+//            System.out.println("city is noida");
+//        }else if(city == "Agra") {
+//            System.out.println("city is agra");
+//        }else {
+//            System.out.println(city);
+//        }
+//        int num = 5;
+//        switch (num){
+//            case 0:
+//                System.out.println("number is 0");
+//                break;
+//            case 1:
+//                System.out.println("number is 1");
+//                break;
+//            case 2:
+//                System.out.println("number is 2");
+//                break;
+//            default:
+//                System.out.println("Enter Number First!!!");
+//        }
+
+//        int[] arr = new int[5];
+//        arr[0] = 100;
+//        arr[1] = 200;
+//        arr[2] = 300;
+//        arr[3] = 400;
+//        arr[4] = 500;
+        //        System.out.println("Array Display" + arr);
+//        for(int item:arr){
+//            System.out.println("Arr " + item);
+//        }
+
+        ArrayList<String> strArr = new ArrayList<String>();
+        strArr.add("abc");
+        strArr.add("def");
+        strArr.add("xyz");
+        System.out.println("String Array Display: " + strArr);
+        int index = 1;
+        for(String item:strArr){
+            System.out.println("Item " + index + ": " + item);
+            index++;
         }
+//        ArrayList<Integer> dynamicArray = new ArrayList<>();
+//        dynamicArray.add(10);
+//        dynamicArray.add(20);
+//        dynamicArray.add(30);
+//        dynamicArray.add(40);
+//        dynamicArray.add(50);
+//        System.out.println("Arr" + dynamicArray);
+//
+//        for(int i = 0 ; i< dynamicArray.size(); i++){
+//            System.out.println("get Index " + i + " Array " + dynamicArray.get(i));
+//        }
     }
 }
