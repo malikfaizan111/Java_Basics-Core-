e7a43542f30b69da013ccbb8c24ceffc332bd691
Faizan Atif <faizan.atif@ircmonline.com>
2023-12-12 17:44:35 +0500

    update task

---
+++ Java Basics/java Core/src/LocalInner/LocalInner.java
@@ -0,0 +1,44 @@
+package LocalInner;
+
+abstract class My {
+    abstract public void myMethod();
+}
+
+interface InFace {
+    abstract public void InFaceMethod();
+}
+
+class Outer {
+    public void display() {
+        class Inner {
+            public void show() {
+                System.out.println("Hello");
+            }
+        }
+        new Inner().show();
+        // Inner i = new Inner();
+        // i.show();
+    }
+
+    public void abstractLocal() {
+        new My() {
+            public void myMethod() {
+                System.out.println("Hello Override anonyms Inner Classs");
+            }
+        }.myMethod();
+        // you can do same in inteface
+        new InFace() {
+            public void InFaceMethod() {
+                System.out.println("Hello Override anonyms Inner Interface object");
+            }
+        }.InFaceMethod();
+    }
+}
+
+public class LocalInner {
+    public static void main(String[] args) {
+        Outer out = new Outer();
+        out.display();
+        out.abstractLocal();
+    }
+}
---
+++ Udemy/2) Data Types- Variables and Literals/DataSizeRange.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+public class DataSizeRange {
+    public static void main(String[] args) {
+        int x; // Primitive DataTypes
+        Integer y;// classes around primitive dataTypes
+
+        int z = -5;
+        System.out.println("Binary from int= "+ Integer.toBinaryString(z));
+    }
+}
